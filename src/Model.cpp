30987cfde0421bcdc35f4b98fcdd3e2ab0e25dad
Shivamshan Sivanesan <116385693+SShivamshan@users.noreply.github.com>
2025-05-09 21:07:43 +0200

    Update Model.cpp

--- src/Model.cpp
+++ src/Model.cpp
@@ -2,14 +2,15 @@
 using namespace Model;
 
 
-MobileViTClassifierImpl::MobileViTClassifierImpl(int img_size, std::vector<int> features_list, std::vector<int> d_list,
+// ---------------------------------------------------------------- MobileViT Classifier ---------------------------------------------------------------- //  
+MobileViTClassifierImpl::MobileViTClassifierImpl(std::vector<int> features_list, std::vector<int> d_list,
     std::vector<int> transformer_depth, int expansion,int num_classes){
     
-    _mobilevit = register_module("MobileViT", MobileViT(img_size,features_list, d_list, transformer_depth, expansion));
-    int convnext_output_channels = features_list.back() * 7 * 7; 
+    _mobilevit = register_module("MobileViT", MobileViT(features_list, d_list, transformer_depth, expansion));
+    int convnext_output_channels = features_list.back(); 
     _fc = register_module("FC", torch::nn::Linear(convnext_output_channels, 512));
     _batchnorm = register_module("BatchNorm", torch::nn::BatchNorm1d(torch::nn::BatchNorm1dOptions(512)));
-    _dropout = register_module("Dropout1", torch::nn::Dropout(torch::nn::DropoutOptions(0.25)));
+    _dropout = register_module("Dropout", torch::nn::Dropout(torch::nn::DropoutOptions(0.25)));
     _classifier = register_module("Classifier", torch::nn::Linear(512, num_classes));
 }
 
@@ -23,6 +24,8 @@ torch::Tensor MobileViTClassifierImpl::forward(torch::Tensor x){
     fc_output = _dropout(torch::relu(fc_output));                      
     return _classifier(fc_output);
 }
+
+// ---------------------------------------------------------------- ConvNeXt Classifier ---------------------------------------------------------------- // 
 
 ConvNextClassifierImpl::ConvNextClassifierImpl(int num_classes,const int image_channel,bool use_tiny)
 {   
@@ -51,9 +54,6 @@ ConvNextClassifierImpl::ConvNextClassifierImpl(int num_classes,const int image_c
     dropout2 = register_module("Dropout2", torch::nn::Dropout(torch::nn::DropoutOptions(0.3)));
     classifier = register_module("Classifier", torch::nn::Linear(512, num_classes));
 
-    // Initialize the classifier head
-    // torch::nn::init::trunc_normal_(classifier->weight, 0.02);
-    // torch::nn::init::zeros_(classifier->bias);
 }
 
 torch::Tensor ConvNextClassifierImpl::forward(torch::Tensor x)
@@ -61,19 +61,19 @@ torch::Tensor ConvNextClassifierImpl::forward(torch::Tensor x)
     x = convnext->forward(x);                  // (B, C, H, W)
     x = x.permute({0, 2, 3, 1}).contiguous();  // (B, H, W, C)
     x = norm->forward(x);                    
-    x = x.permute({0, 3, 1, 2}).contiguous();  // Back to (B, C, H, W)
+    x = x.permute({0, 3, 1, 2}).contiguous();  // (B, C, H, W)
 
     x = avgpool->forward(x);                   // (B, C, 1, 1)
     x = x.flatten(1);                          // (B, C)
     x = dropout1->forward(x);
     x = fc->forward(x);
-    x = torch::gelu(x);                        // GELU non-linearity
+    x = torch::gelu(x);                        
     x = dropout2->forward(x);
     x = classifier->forward(x);
     return x;      
 }
 
-
+// ---------------------------------------------------------------- Linear Classifier ---------------------------------------------------------------- // 
 ClassifierImpl::ClassifierImpl(const size_t image_size, std::vector<int> in_channels, const int num_classes)
     : dropout(0.2)
 {
@@ -95,5 +95,66 @@ torch::Tensor ClassifierImpl::forward(torch::Tensor x) {
     x = dropout(torch::relu(fc3->forward(x)));
     x = dropout(torch::relu(fc4->forward(x)));
     x = fc5->forward(x); 
+    return x;
+}
+
+// ---------------------------------------------------------------- CNN Classifier ---------------------------------------------------------------- // 
+
+CNNClassifierImpl::CNNClassifierImpl(std::vector<int> channels, const int num_classes) {
+    TORCH_CHECK(channels.size() == 5, "Expected 5 elements in channels vector");
+
+    cnn_block1 = register_module("cnn_block1", make_cnn_blocks(3, channels[0], 1, 1, "block1"));
+    cnn_block2 = register_module("cnn_block2", make_cnn_blocks(channels[0], channels[1], 1, 1, "block2"));
+    cnn_block3 = register_module("cnn_block3", make_cnn_blocks(channels[1], channels[2], 1, 1, "block3"));
+    cnn_block4 = register_module("cnn_block4", make_cnn_blocks(channels[2], channels[3], 1, 1, "block4"));
+    cnn_block5 = register_module("cnn_block5", make_cnn_blocks(channels[3], channels[4], 1, 1, "block5"));
+
+    avgpool = register_module("avgpool", torch::nn::AdaptiveAvgPool2d(torch::nn::AdaptiveAvgPool2dOptions({7, 7})));
+    max_pool = register_module("max_pool",torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2).stride(2)));
+    _fc = register_module("_fc", torch::nn::Linear(channels[4]*7*7, 512));
+    _batchnorm = register_module("_batchnorm", torch::nn::BatchNorm1d(512));
+    _dropout1 = register_module("_dropout1", torch::nn::Dropout(0.25));
+    _dropout2 = register_module("_dropout2", torch::nn::Dropout(0.3));
+    _classifier = register_module("_classifier", torch::nn::Linear(512, num_classes));
+}
+
+torch::nn::Sequential CNNClassifierImpl::make_cnn_blocks(int in_channels, int out_channels, int stride, int padding, const std::string& block_name) {
+    torch::nn::Sequential block;
+
+    auto conv = torch::nn::Conv2d(torch::nn::Conv2dOptions(in_channels, out_channels, 3)
+                                    .stride(stride).padding(padding).bias(false));
+    auto bn = torch::nn::BatchNorm2d(out_channels);
+    auto relu = torch::nn::ReLU(torch::nn::ReLUOptions(true));
+
+    block->push_back(register_module(block_name + "_conv", conv));
+    block->push_back(register_module(block_name + "_bn", bn));
+    block->push_back(relu);
+
+    return block;
+}
+
+torch::Tensor CNNClassifierImpl::forward(torch::Tensor x) {
+    x = cnn_block1->forward(x);
+    x = max_pool->forward(x);
+    x = _dropout1->forward(x);
+    x = cnn_block2->forward(x);
+    x = max_pool->forward(x);
+    x = _dropout1->forward(x);
+    x = cnn_block3->forward(x);
+    x = max_pool->forward(x);
+    x = _dropout1->forward(x);
+    x = cnn_block4->forward(x);
+    x = max_pool->forward(x);
+    x = _dropout1->forward(x);
+    x = cnn_block5->forward(x);
+
+    x = avgpool->forward(x);
+    x = _dropout1->forward(x);
+    x = x.view({x.size(0), -1});  
+    x = _fc->forward(x);
+    x = _batchnorm->forward(x);
+    x = torch::relu(x);
+    x = _dropout2->forward(x);
+    x = _classifier->forward(x);
     return x;
 }
